8380b67323d0cd12cb1028252e43231b18f0e571
Nilangshu Majhi (monst8r) <67144582+monster8d@users.noreply.github.com>
2022-08-19 09:57:17 +0530

    Add files via upload

---
+++ octavi_RMX1901.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Evolution stuff
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+
+# Inherit from RMX1901 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := Realme
+PRODUCT_DEVICE := RMX1901
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_NAME := octavi_RMX1901
+PRODUCT_MODEL := Realme X
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys" \
+    PRODUCT_NAME="RMX1901"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
